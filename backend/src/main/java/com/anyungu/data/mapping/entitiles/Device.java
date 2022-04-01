21034b6c2e7e12f572ecc34d8dbe6f28c1482808
Adams Okode <adamsokode@gmail.com>
2022-04-01 19:12:43 +0300

    UUID Generation

--- backend/src/main/java/com/anyungu/data/mapping/entitiles/Device.java
+++ backend/src/main/java/com/anyungu/data/mapping/entitiles/Device.java
@@ -3,8 +3,13 @@ package com.anyungu.data.mapping.entitiles;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import java.util.UUID;
 
 @Entity(name = "device")
 @Getter
@@ -13,11 +18,15 @@ import javax.persistence.*;
 public class Device {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, unique = true)
-    private Integer id;
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "device_uuid", updatable = false, nullable = false, unique = true)
+    private UUID deviceUuid;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "serial_number", nullable = false)
     private String serialNumber;
 
     @Column(name = "name", nullable = false)
@@ -25,7 +34,6 @@ public class Device {
 
     @Column(name = "battery_level", nullable = false)
     private Integer batteryLevel;
-
 
 
 }
--- backend/src/main/java/com/anyungu/data/mapping/entitiles/DeviceData.java
+++ backend/src/main/java/com/anyungu/data/mapping/entitiles/DeviceData.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -15,15 +17,30 @@ import java.time.LocalDateTime;
 public class DeviceData {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, unique = true)
-    private Integer id;
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "uuid", nullable = false, unique = true)
+    private UUID id;
 
+    @Column(name = "latitude", nullable = false)
     private Float latitude;
+
+    @Column(name = "longitude", nullable = false)
     private Float longitude;
+
+    @Column(name = "temp", nullable = false)
     private Float temp;
+
+    @Column(name = "humidity", nullable = false)
     private Float humidity;
+
+    @Column(name = "timestamp", nullable = false)
     private LocalDateTime timestamp;
-    private Integer deviceId;
+
+    @Column(name = "device_id", nullable = false, unique = true)
+    private UUID deviceId;
     
 }

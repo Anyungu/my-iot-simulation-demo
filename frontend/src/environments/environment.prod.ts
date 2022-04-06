17460ad99a8a3ff3b0c5394075ece568a4321cc6
Anyungu <anyungucw@gmail.com>
2022-04-06 19:15:37 +0300

    CU-257uwzp Map responsive (#29)
    * responsive
    
    * responsive-kidogo

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://iot-api.kodosensei.com',
+  apiUrl: 'https://iot-api.kodosensei.com/api/v1',
+  demoUser: 'admin',
+  demoPassword: 'Kurosaki8'
 };

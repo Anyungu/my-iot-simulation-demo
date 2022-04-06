755767cd65dd950f4efdab3044bc4c684a7dcd1d
Adams Okode <adamsokode@gmail.com>
2022-04-06 10:11:49 +0300

    CU-25daje9 - Create Password Reset Models

---
+++ backend/src/main/java/com/anyungu/data/mapping/entities/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.anyungu.data.mapping.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    @Column(name = "deleted")
+    private boolean deleted = false;
+}
